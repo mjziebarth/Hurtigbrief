26d886bc23c7be43698a2e4f4e4f7d7bf26d3d95
Malte Jörn Ziebarth <malte.ziebarth@fmvkb.de>
2023-02-04 15:21:52 +0100

    Add the opening field.

--- hurtigbrief/gui/window.py
+++ hurtigbrief/gui/window.py
@@ -66,18 +66,24 @@ class HurtigbriefWindow(Gtk.ApplicationWindow):
         self.subject_edit = GtkSource.View(buffer=self.subject_buffer)
         layout_left.attach(self.subject_edit, 0, 2, 2, 1)
 
+        # The opening:
+        self.opening_buffer = GtkSource.Buffer(language=language)
+        self.opening_buffer.set_text(default['opening'])
+        self.opening_edit = GtkSource.View(buffer=self.opening_buffer)
+        layout_left.attach(self.opening_edit, 0, 3, 2, 1)
+
         # The main body text source view:
         bodyscroll = Gtk.ScrolledWindow()
         self.body_buffer = GtkSource.Buffer(language=language)
         self.body_edit = GtkSource.View(buffer=self.body_buffer, expand = True)
         bodyscroll.add(self.body_edit)
-        layout_left.attach(bodyscroll, 0, 3, 2, 1)
+        layout_left.attach(bodyscroll, 0, 4, 2, 1)
 
         # The closing:
         self.closing_buffer = GtkSource.Buffer(language=language)
         self.closing_buffer.set_text(default['closing'])
         self.closing_edit = GtkSource.View(buffer=self.closing_buffer)
-        layout_left.attach(self.closing_edit, 0, 4, 2, 1)
+        layout_left.attach(self.closing_edit, 0, 5, 2, 1)
 
         # The PDF view:
         self.pdf_view = EvinceView.View(expand = True)
@@ -86,7 +92,7 @@ class HurtigbriefWindow(Gtk.ApplicationWindow):
         layout.add2(evscroll)
 
         run_button = Gtk.Button("Go!")
-        layout_left.attach(run_button, 0, 5, 1, 1)
+        layout_left.attach(run_button, 0, 6, 1, 1)
         run_button.connect("clicked", lambda btn : self.generate_letter())
 
         self.add(layout)

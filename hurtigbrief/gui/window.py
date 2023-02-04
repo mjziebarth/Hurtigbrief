8dd7b96d4154c72f4d8841fc05fc703563899844
Malte Jörn Ziebarth <malte.ziebarth@fmvkb.de>
2023-02-04 15:05:20 +0100

    Read some more config and start generate letter WIP for development.

--- hurtigbrief/gui/window.py
+++ hurtigbrief/gui/window.py
@@ -15,14 +15,35 @@
 
 from .gtk import Gtk, GtkSource, EvinceView
 from .config import default
+from ..abstraction.address import address_from_json
+from ..abstraction.person import Person
+from ..abstraction.letter import Letter
+from typing import Optional
 
 class HurtigbriefWindow(Gtk.ApplicationWindow):
+    """
+    The window.
+    """
+    default_sender: Optional[int]
 
     def __init__(self, application=None):
         super().__init__(application=application)
 
         # Some variables:
-        self.people = []
+        self.addresses = [address_from_json(json)
+                          for json in default["addresses"]]
+        self.people = [Person.from_json(json, self.addresses)
+                       for json in default["people"]]
+        default_sender = int(default["default_sender"])
+        if default_sender > 0 and default_sender < len(self.people):
+            self.sender = default_sender
+        else:
+            self.sender = None
+        print("addresses:", self.addresses)
+
+        # TODO fixme!
+        self.destination = 1
+
 
         # Layout
         layout = Gtk.HPaned()
@@ -32,24 +53,31 @@ class HurtigbriefWindow(Gtk.ApplicationWindow):
         # The addresses:
         label_sender = Gtk.Label('From:', halign=Gtk.Align.START)
         layout_left.attach(label_sender, 0, 0, 1, 1)
+        label_destination = Gtk.Label('To:', halign=Gtk.Align.START)
+        layout_left.attach(label_destination, 0, 1, 1, 1)
 
-        # The main body text source view:
+        # The subject:
         try:
             languages = GtkSource.LanguageManager()
             language = languages.get_language('latex')
         except:
             language = None
+        self.subject_buffer = GtkSource.Buffer(language=language)
+        self.subject_edit = GtkSource.View(buffer=self.subject_buffer)
+        layout_left.attach(self.subject_edit, 0, 2, 2, 1)
+
+        # The main body text source view:
         bodyscroll = Gtk.ScrolledWindow()
         self.body_buffer = GtkSource.Buffer(language=language)
         self.body_edit = GtkSource.View(buffer=self.body_buffer, expand = True)
         bodyscroll.add(self.body_edit)
-        layout_left.attach(bodyscroll, 0, 1, 2, 1)
+        layout_left.attach(bodyscroll, 0, 3, 2, 1)
 
         # The closing:
         self.closing_buffer = GtkSource.Buffer(language=language)
         self.closing_buffer.set_text(default['closing'])
         self.closing_edit = GtkSource.View(buffer=self.closing_buffer)
-        layout_left.attach(self.closing_edit, 0, 2, 2, 1)
+        layout_left.attach(self.closing_edit, 0, 4, 2, 1)
 
         # The PDF view:
         self.pdf_view = EvinceView.View(expand = True)
@@ -57,7 +85,9 @@ class HurtigbriefWindow(Gtk.ApplicationWindow):
         evscroll.add(self.pdf_view)
         layout.add2(evscroll)
 
-        layout_left.attach(Gtk.Button("blabla"), 0, 3, 1, 1)
+        run_button = Gtk.Button("Go!")
+        layout_left.attach(run_button, 0, 5, 1, 1)
+        run_button.connect("clicked", lambda btn : self.generate_letter())
 
         self.add(layout)
 
@@ -68,3 +98,14 @@ class HurtigbriefWindow(Gtk.ApplicationWindow):
         font_height = label_sender.get_allocated_height()
 
         layout_left.set_row_spacing(max(round(0.2*font_height), 1))
+
+    def generate_letter(self) -> Letter:
+        """
+        Generates the letter from the current content.
+        """
+        print("generate letter!")
+        return Letter(self.people[self.sender], self.people[self.destination],
+                      self.subject_buffer.get_text(),
+                      self.opening_buffer.get_text(),
+                      self.body_buffer.get_text(),
+                      self.closing_buffer.get_text())

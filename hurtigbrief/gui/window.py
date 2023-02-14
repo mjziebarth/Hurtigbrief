17ac58e9794ee540795a401b1d2df24d6a9d49c7
Malte J. Ziebarth <mjz.science@fmvkb.de>
2023-02-15 00:09:26 +0100

    Emit letter generation whenever letter content change is registered.

--- hurtigbrief/gui/window.py
+++ hurtigbrief/gui/window.py
@@ -78,18 +78,21 @@ class HurtigbriefWindow(Gtk.ApplicationWindow):
         except:
             language = None
         self.subject_buffer = GtkSource.Buffer(language=language)
+        self.subject_buffer.connect('changed', self.on_letter_changed)
         self.subject_edit = GtkSource.View(buffer=self.subject_buffer)
         layout_left.attach(self.subject_edit, 0, 2, 2, 1)
 
         # The opening:
         self.opening_buffer = GtkSource.Buffer(language=language)
         self.opening_buffer.set_text(default['opening'])
+        self.opening_buffer.connect('changed', self.on_letter_changed)
         self.opening_edit = GtkSource.View(buffer=self.opening_buffer)
         layout_left.attach(self.opening_edit, 0, 3, 2, 1)
 
         # The main body text source view:
         bodyscroll = Gtk.ScrolledWindow()
         self.body_buffer = GtkSource.Buffer(language=language)
+        self.body_buffer.connect('changed', self.on_letter_changed)
         self.body_edit = GtkSource.View(buffer=self.body_buffer, expand = True)
         bodyscroll.add(self.body_edit)
         layout_left.attach(bodyscroll, 0, 4, 2, 1)
@@ -97,6 +100,7 @@ class HurtigbriefWindow(Gtk.ApplicationWindow):
         # The closing:
         self.closing_buffer = GtkSource.Buffer(language=language)
         self.closing_buffer.set_text(default['closing'])
+        self.closing_buffer.connect('changed', self.on_letter_changed)
         self.closing_edit = GtkSource.View(buffer=self.closing_buffer)
         layout_left.attach(self.closing_edit, 0, 5, 2, 1)
 
@@ -122,7 +126,7 @@ class HurtigbriefWindow(Gtk.ApplicationWindow):
 
         layout_left.set_row_spacing(max(round(0.2*font_height), 1))
 
-    def generate_letter(self) -> Letter:
+    def generate_letter(self):
         """
         Generates the letter from the current content.
         """
@@ -153,6 +157,12 @@ class HurtigbriefWindow(Gtk.ApplicationWindow):
                   Letter(sender, destination, subject, opening, body, closing),
                   Design(),
                   "scrletter")
+
+    def on_letter_changed(self, *args):
+        """
+        General purpose slot called when the letter is changed.
+        """
+        self.generate_letter()
 
     def on_receive_result(self, result: TaskResult):
         """
